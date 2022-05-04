cbf74d28618c16b236bafc1083dbe2b26e389f7d
prashantfs <85509055+prashantfs@users.noreply.github.com>
2022-05-04 11:29:36 +0530

    crypto updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// import { scryptSync,createCipheriv } from 'node:crypto'
+const crypto = require('crypto')
+const CryptoJS = require("crypto-js")
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myApp';
+  title = 'Crypto';
+  finalValue = '';
+  encryptionObj = {
+    string: '',
+    key: 'Password used to generate key' 
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  public encryptVariables = () => {
+    const key = this.encryptionObj.key
+    const iv = Buffer.alloc(16, 0)
+    
+    const cipher = CryptoJS.AES.encrypt(this.encryptionObj.string, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(iv), // parse the IV 
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    })
+  
+    console.log(cipher.toString())
+    this.finalValue = cipher.toString()
+    alert(cipher.toString())
+  }
+
+  public changeValue(type: Number, value: any) {
+    if(type === 1) {
+      this.encryptionObj.string = value.value
+    } else {
+      this.encryptionObj.key = value.value
+    }
+  }
+
+  public decryptVariables = () => {
+    const key = this.encryptionObj.key
+    const iv = Buffer.alloc(16, 0)
+    
+    const cipher = CryptoJS.AES.decrypt(this.encryptionObj.string, key, {
+        iv: CryptoJS.enc.Utf8.parse(iv), // parse the IV 
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    })
+    
+    // e.g. B6AeMHPHkEe7/KHsZ6TW/Q==
+    console.log(cipher.toString())
+    this.finalValue = cipher.toString()
+    alert(cipher.toString())
+  }
+
 }
